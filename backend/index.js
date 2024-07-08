e7bc0abf8989d7ae98ac3f32c089b3fb3f9d95c6
baijanathTharu <bnchaudhary.tharu@gmail.com>
2024-07-08 22:22:42 +0545

    init frontend app

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,14 @@
 const PORT = 5000;
 
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
+app.use(cors());
+
 app.get("/", function (req, res) {
-  res.send("Hello World");
+  res.json({ message: "Hello from backend v1!!" });
 });
 
 const server = app.listen(PORT, () => {
